82af0e8b00616dcce046448d72d9747bf66a650d
wiichog <gar14189@uvg.edu.gt>
2016-09-18 01:47:09 -0600

    Changed maded on eclouse but is not working

--- SimulacionAFN.java
+++ SimulacionAFN.java
@@ -3,26 +3,25 @@ public class SimulacionAFN{
 	
 	public AFN ECerraduraEstado(AFN afn){
 		ArrayList<Transicion> Transiciones = afn.GetCaminos();
-		
+		ArrayList<Integer> CaminosFinales = new ArrayList<Integer>();
 		Stack st = new Stack();
 		for(int i=0;i<Transiciones.size();i++){
 			Transicion Camino = Transiciones.get(i);
 			int EstadoInicial = Camino.GetEstadoInicial();
+			//System.out.println("EstadoInicial " +EstadoInicial);	
 			int EstadoFinal = Camino.GetEstadoFinal();
-			System.out.println("****************");
+			//recorremos el array para encontrar todos los finales
+			//vaciamos antes la lista 
+			CaminosFinales.clear();
 			for(int j=0;j<Transiciones.size();j++){
 				Transicion Caminos = Transiciones.get(j);
-				if((Caminos.GetEstadoInicial()==EstadoFinal || Caminos.GetEstadoInicial()==EstadoInicial) && (Caminos.GetSimbolo().equals("«"))){
-					System.out.println("EstadoFinal   " + Caminos.GetEstadoFinal());
-					System.out.println("Simbolo  " + Caminos.GetSimbolo());
-					st.push(Caminos);
+				if ((Caminos.GetEstadoInicial()==EstadoInicial) && (Caminos.GetSimbolo().equals("«"))){
+					System.out.println("EstadoFinales" + Caminos.GetEstadoFinal());
+					CaminosFinales.add(Caminos.GetEstadoFinal());
 				}
-
 			}
-		
-				
+			Transiciones.remove(i);
 			}
 			return null;
 		}
 	}
-	
